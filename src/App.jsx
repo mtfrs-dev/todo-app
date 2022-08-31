4d7c06abe3566576d5295ed004cd7e86444db5de
Togi <markus.togi@gmail.com>
2022-09-01 02:53:12 +0700

    Initialize Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./components/Welcome"
+import "./assets/css/index.css"
+
+export default function App() {
+    return (
+        <>
+            <Welcome />
+        </>
+    )
+}
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <>
+            <div className="wrapper">
+                <h1 className="text-4xl font-bold">Welcome To My App</h1>
+            </div>
+        </>
+    );
+}
